3afef4442745643afde1ce247f0703636ce0e477
SimonNguthu <simonnguthuM@gmail.com>
2022-07-04 19:45:56 -0700

    .

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,22 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)siezof(a));
+	printf("Size of a int: %lu byte(s)\n", (unsigned long)siezof(b));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)siezof(c));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)siezof(d));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)siezof(e));
+	return (0);
+}
